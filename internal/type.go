6cd7f3ffb6b48ee4de6f120a4e13ea2a9d713c50
Jeff Haynie <jhaynie@pinpoint.com>
2020-06-02 11:01:25 -0500

    add issue type mapping mapped type

--- internal/type.go
+++ internal/type.go
@@ -4,6 +4,30 @@ import (
 	"github.com/pinpt/agent.next/sdk"
 )
 
+func getMappedIssueType(name string, subtask bool) sdk.WorkIssueTypeMappedType {
+	if subtask {
+		// any subtask will have this flag set
+		return sdk.WorkIssueTypeMappedTypeSubtask
+	}
+	// map out of the box jira types that are known
+	switch name {
+	case "Story":
+		return sdk.WorkIssueTypeMappedTypeStory
+	case "Improvement", "Enhancement":
+		return sdk.WorkIssueTypeMappedTypeEnhancement
+	case "Epic":
+		return sdk.WorkIssueTypeMappedTypeEpic
+	case "New Feature":
+		return sdk.WorkIssueTypeMappedTypeFeature
+	case "Bug":
+		return sdk.WorkIssueTypeMappedTypeBug
+	case "Task":
+		return sdk.WorkIssueTypeMappedTypeTask
+	}
+	// otherwise this is a custom type which can be mapped from the app side by the user
+	return sdk.WorkIssueTypeMappedTypeUnknown
+}
+
 func (t issueType) ToModel(customerID string) (*sdk.WorkIssueType, error) {
 	issuetype := &sdk.WorkIssueType{}
 	issuetype.CustomerID = customerID
@@ -12,6 +36,7 @@ func (t issueType) ToModel(customerID string) (*sdk.WorkIssueType, error) {
 	issuetype.Name = t.Name
 	issuetype.Description = sdk.StringPointer(t.Description)
 	issuetype.IconURL = sdk.StringPointer(t.Icon)
+	issuetype.MappedType = getMappedIssueType(t.Name, t.Subtask)
 	issuetype.ID = sdk.NewWorkIssueTypeID(customerID, refType, t.ID)
 	return issuetype, nil
 }
