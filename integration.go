b4e7e90d1d817e685b5f0dfb8a55d1c391fe217e
Robindiddams <rdiddams@pinpoint.com>
2020-09-13 19:52:28 -0500

    Revert "add stacktraces thing"
    This reverts commit 18da5a462d9a85930d1cbaa2e6d0f0aa30f99ca2.

--- integration.go
+++ integration.go
@@ -3,9 +3,6 @@ package main
 import (
 	"github.com/pinpt/agent.next.jira/internal"
 	"github.com/pinpt/agent.next/runner"
-
-	// so we can dump the stack!
-	_ "github.com/songgao/stacktraces/on/SIGUSR2"
 )
 
 // Integration is used to export the integration
