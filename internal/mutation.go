a7e8975bf727acf8d7bcbc316f8b771110beca57
Pedro Enrique <penrique@pinpoint.com>
2020-10-02 15:33:50 +0200

    update agent to 4.0.2

--- internal/mutation.go
+++ internal/mutation.go
@@ -42,7 +42,7 @@ func newMutation() mutationRequest {
 }
 
 // Mutation is called when a mutation request is received on behalf of the integration
-func (i *JiraIntegration) Mutation(mutation sdk.Mutation) error {
+func (i *JiraIntegration) Mutation(mutation sdk.Mutation) (*sdk.MutationResponse, error) {
 	logger := sdk.LogWith(i.logger, "customer_id", mutation.CustomerID(), "id", mutation.ID(), "action", mutation.Action(), "model", mutation.Model())
 	sdk.LogInfo(logger, "mutation request received")
 	user := mutation.User()
@@ -53,27 +53,27 @@ func (i *JiraIntegration) Mutation(mutation sdk.Mutation) error {
 	c.OAuth1Auth = user.OAuth1Auth
 	authConfig, err := i.createAuthConfigFromConfig(mutation, c)
 	if err != nil {
-		return fmt.Errorf("error creating auth config: %w", err)
+		return nil, fmt.Errorf("error creating auth config: %w", err)
 	}
 	switch v := mutation.Payload().(type) {
 	// Issue
 	case *sdk.WorkIssueUpdateMutation:
-		return i.updateIssue(logger, mutation, authConfig, v)
+		return nil, i.updateIssue(logger, mutation, authConfig, v)
 	case *sdk.WorkIssueCreateMutation:
-		return i.createIssue(logger, mutation, authConfig, v)
+		return nil, i.createIssue(logger, mutation, authConfig, v)
 
 	// Sprint
 	case *sdk.AgileSprintUpdateMutation:
 		if !authConfig.SupportsAgileAPI {
-			return errors.New("current authentication does not support agile api")
+			return nil, errors.New("current authentication does not support agile api")
 		}
-		return i.updateSprint(logger, mutation, authConfig, v)
+		return nil, i.updateSprint(logger, mutation, authConfig, v)
 	case *sdk.AgileSprintCreateMutation:
 		if !authConfig.SupportsAgileAPI {
-			return errors.New("current authentication does not support agile api")
+			return nil, errors.New("current authentication does not support agile api")
 		}
-		return i.createSprint(logger, mutation, authConfig, v)
+		return nil, i.createSprint(logger, mutation, authConfig, v)
 	}
 	sdk.LogInfo(logger, "unhandled mutation request", "type", reflect.TypeOf(mutation.Payload()))
-	return nil
+	return nil, nil
 }
