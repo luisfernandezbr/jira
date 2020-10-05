e2451d3f7a4aa3a8ff5ce7715ec4a48e983e0973
Robindiddams <rdiddams@pinpoint.com>
2020-10-05 15:43:59 -0500

    dont fail if we cant get the name from the index ⛑

--- internal/validate.go
+++ internal/validate.go
@@ -28,7 +28,8 @@ type projectSearchResult struct {
 // Validate will perform pre-installation operations on behalf of the UI
 func (i *JiraIntegration) Validate(validate sdk.Validate) (map[string]interface{}, error) {
 	config := validate.Config()
-	sdk.LogDebug(i.logger, "Validate", "config", config)
+	logger := sdk.LogWith(i.logger)
+	sdk.LogDebug(logger, "Validate", "config", config)
 	found, action := config.GetString("action")
 	if !found {
 		return nil, fmt.Errorf("validation had no action")
@@ -62,45 +63,43 @@ func (i *JiraIntegration) Validate(validate sdk.Validate) (map[string]interface{
 		qs.Set("status", "live")
 		qs.Set("typeKey", "software")
 		var resp projectSearchResult
+		sdk.LogDebug(logger, "fetching project count")
 		r, err := client.Get(&resp, append(authConfig.Middleware, sdk.WithGetQueryParameters(qs))...)
 		if err != nil {
 			if httperr, ok := err.(*sdk.HTTPError); ok {
 				buf, _ := ioutil.ReadAll(httperr.Body)
-				sdk.LogError(i.logger, "error reading data for validate", "buf", string(buf))
+				sdk.LogError(logger, "error reading data for validate", "buf", string(buf))
 			}
 			return nil, fmt.Errorf("error fetching project accounts: %w", err)
 		}
 		if r.StatusCode != http.StatusOK {
-			sdk.LogDebug(i.logger, "unusual status code", "code", r.StatusCode)
+			sdk.LogDebug(logger, "unusual status code", "code", r.StatusCode)
 		}
-		indexURL := sdk.JoinURL(authConfig.APIURL, "/")
-		client = i.httpmanager.New(indexURL, nil)
-		var resp2 interface{}
-		r, err = client.Get(&resp2, authConfig.Middleware...)
-		if err != nil {
+		var name, avatar string
+		client = i.httpmanager.New(sdk.JoinURL(authConfig.APIURL, "/"), nil)
+		if r, err = client.Get(nil, authConfig.Middleware...); err == nil {
+			// try and extract the name if possible
+			re := regexp.MustCompile(`<meta name="ajs-cloud-name" content="(.*?)">`)
+			nametok := re.FindStringSubmatch(string(r.Body))
+			if len(nametok) > 0 {
+				name = nametok[1]
+			}
+			re = regexp.MustCompile(`<link rel="shortcut icon" href="(.*?)">`)
+			avatartok := re.FindStringSubmatch(string(r.Body))
+			if len(avatartok) > 0 {
+				avatar = sdk.JoinURL(authConfig.APIURL, avatartok[1])
+			}
+		} else {
 			if httperr, ok := err.(*sdk.HTTPError); ok {
 				buf, _ := ioutil.ReadAll(httperr.Body)
-				sdk.LogError(i.logger, "error reading data for validate (index)", "buf", string(buf))
+				sdk.LogError(logger, "error reading data for validate (index)", "buf", string(buf))
 			}
-			return nil, fmt.Errorf("error fetching project accounts: %w", err)
-		}
-		// try and extract the name if possible
-		re := regexp.MustCompile(`<meta name="ajs-cloud-name" content="(.*?)">`)
-		nametok := re.FindStringSubmatch(string(r.Body))
-		var name string
-		if len(nametok) > 0 {
-			name = nametok[1]
+			sdk.LogError(logger, "error fetching account name", "err", err)
 		}
 		if name == "" {
 			i := strings.Index(authConfig.APIURL, "://")
 			tok := strings.Split(authConfig.APIURL[i+3:], ".")
 			name = tok[0]
-		}
-		re = regexp.MustCompile(`<link rel="shortcut icon" href="(.*?)">`)
-		var avatar string
-		avatartok := re.FindStringSubmatch(string(r.Body))
-		if len(avatartok) > 0 {
-			avatar = sdk.JoinURL(authConfig.APIURL, avatartok[1])
 		}
 		acc := sdk.ValidatedAccount{
 			ID:          authConfig.APIURL,
